4935b8bd8d6893be2cb1985a7b55da7ffffa0c31
KittyHalfMoon <catscare2539@hotmail.com>
2023-01-13 19:31:26 +0700

    add all files

---
+++ app.js
@@ -0,0 +1,29 @@
+// chalk
+const chalk = require('chalk')          //ปรับสีได้
+// debug
+const debug = require('debug')('app');
+// morgan
+const morgan = require('morgan')
+// app
+const express = require('express');
+const app = express();
+// port
+const port = 3000;
+
+app.use(morgan('combined'))
+
+app.get("/",(req,res)=>{
+    res.send( '<h1>hello world</h1>')
+})
+
+app.listen(port, ()=>{
+    console.log('Lisening Port',chalk.red(port)); //just plain txt
+    debug('Lisening Port',port); 
+})
+
+
+
+
+
+
+// app.use(morgan('combined'))
